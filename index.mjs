e38c27980fd198ef351762870af67bba24c4fddc
Shivam <vg0292113@gmail.com>
2026-03-24 15:54:32 +0530

    root route

--- index.mjs
+++ index.mjs
@@ -1,9 +1,15 @@
 import express from 'express'
 const server = express()
 
-const port = 5000
+const port = 3000;
+server.get("/", (req, res) => {
+    res.json({ name: "apple" })
+})
 
-server.get("/", (rep))
-server.listen(3000, () => {
+server.get("/hello", (req, res) => {
+    res.json({ name: "banana" })
+})
+
+server.listen(port, () => {
     console.log('server started on ${port}')
 })
